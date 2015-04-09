9d22cb7393ea5dec64359b2de2123982e627e2ef
cooooper <ydhan@ideabinder.com>
2015-04-09 18:34:31 +0800

    first commit

---
+++ WatchKitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchKitDemo
+//
+//  Created by Han Yahui on 15/4/2.
+//  Copyright (c) 2015年 Han Yahui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
